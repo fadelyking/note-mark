3cd119527858e361485a7db29df98183c3689cd2
fadelyking <fadely195@gmail.com>
2024-05-21 10:12:49 +0300

    import sidebar

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,11 @@
 import './assets/index.css'
-
+import Sidebar from './components/Sidebar'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Sidebar />
   </React.StrictMode>
 )
