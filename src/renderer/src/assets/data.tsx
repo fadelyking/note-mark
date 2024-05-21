0b4133573dcbd37638e80ca8d93a1839a8e29314
fadelyking <fadely195@gmail.com>
2024-05-21 10:12:24 +0300

    dummy data

---
+++ src/renderer/src/assets/data.tsx
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    title: 'my first note',
+    content: 'today I had a great day tbh'
+  },
+  {
+    title: 'my second note',
+    content: 'today I had a bad day tbh'
+  },
+  {
+    title: 'my third note',
+    content: 'today I had a sad day tbh'
+  },
+  {
+    title: 'my fourth note',
+    content: 'today I had a okay day tbh'
+  }
+]
+
+export { notes }
