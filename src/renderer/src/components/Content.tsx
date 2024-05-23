2a9f50f718bb7c6d45f6d2466e32176f31081f39
fadelyking <fadely195@gmail.com>
2024-05-23 10:50:42 +0300

    add children to content

--- src/renderer/src/components/Content.tsx
+++ src/renderer/src/components/Content.tsx
@@ -1,3 +1,3 @@
-export default function Content() {
-  return <div className="grow bg-slate-300 p-5">test test</div>
+export default function Content({ children }) {
+  return <div className="grow bg-slate-300 p-5">{children}</div>
 }
