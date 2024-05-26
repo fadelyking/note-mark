e7e70fbea3b21bbd7eebc9f48f89a47828fbd008
fadelyking <fadely195@gmail.com>
2024-05-26 12:25:38 +0300

    remove imports from sidebar

--- src/renderer/src/components/Sidebar.tsx
+++ src/renderer/src/components/Sidebar.tsx
@@ -1,6 +1,3 @@
-import { notes } from '@renderer/assets/data'
-import { v4 as uuidv4 } from 'uuid'
-
 export default function Sidebar({ children }: { children: React.ReactNode }) {
   return <aside className=" overflow-auto w-[250px] h-screen bg-slate-500 p-5">{children}</aside>
 }
