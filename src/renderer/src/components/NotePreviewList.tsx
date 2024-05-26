4896c2c915a5ff81e3d23cb680d64f63fb28225d
fadelyking <fadely195@gmail.com>
2024-05-26 13:41:58 +0300

    edge case when there are no notes

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -2,6 +2,13 @@ import { notes } from '../store/mocks/index'
 import { NotePreview } from './NotePreview'
 import uuid4 from 'uuid4'
 export default function NotesPreviewList() {
+  if (notes.length === 0) {
+    return (
+      <ul className="text-center pt-4">
+        <span>Nothing yet...</span>
+      </ul>
+    )
+  }
   return (
     <ul className="mt-3 space-y-1">
       {notes.map((note) => (
