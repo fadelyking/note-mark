0ed4eeca5e69236f164ae2f42919deff110d452e
fadelyking <fadely195@gmail.com>
2024-05-26 11:30:57 +0300

    mock data

---
+++ src/renderer/src/store/mocks/index.ts
@@ -0,0 +1,22 @@
+import { NoteInfo } from '@shared/models'
+
+const notes: NoteInfo[] = [
+  {
+    title: 'my first note',
+    lastEdited: new Date().getTime()
+  },
+  {
+    title: 'my second note',
+    lastEdited: new Date().getTime()
+  },
+  {
+    title: 'my third note',
+    lastEdited: new Date().getTime()
+  },
+  {
+    title: 'my fourth note',
+    lastEdited: new Date().getTime()
+  }
+]
+
+export { notes }
