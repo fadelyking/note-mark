6e0fa6bbfb155d876744398e7fa1ddf4b48af821
fadelyking <fadely195@gmail.com>
2024-05-26 13:10:15 +0300

    include note component and destructure probs inside

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,10 +1,13 @@
 import { notes } from '../store/mocks/index'
+import { NotePreview } from './NotePreview'
 import uuid4 from 'uuid4'
 export default function NotesPreviewList() {
   return (
     <ul className="mt-3 space-y-1">
       {notes.map((note) => (
-        <li key={uuid4()}>{note.title}</li>
+        <li key={uuid4()}>
+          <NotePreview {...note} />
+        </li>
       ))}
     </ul>
   )
