66ff02ea83e2fc043210268b370d9b9453989033
fadelyking <fadely195@gmail.com>
2024-05-27 15:01:21 +0300

    change base color to zinc

--- src/renderer/src/components/Sidebar.tsx
+++ src/renderer/src/components/Sidebar.tsx
@@ -1,3 +1,3 @@
 export default function Sidebar({ children }: { children: React.ReactNode }) {
-  return <aside className=" overflow-auto w-[250px] h-screen bg-slate-500 p-5">{children}</aside>
+  return <aside className=" overflow-auto w-[250px] h-screen bg-zinc-900 p-5">{children}</aside>
 }
