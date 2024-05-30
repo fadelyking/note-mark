ff61ba7b706e0bbdccc1d817c76e266cac2462f0
fadelyking <fadely195@gmail.com>
2024-05-30 15:25:36 +0300

    padding bottom

--- src/renderer/src/components/Content.tsx
+++ src/renderer/src/components/Content.tsx
@@ -1,3 +1,3 @@
 export default function Content({ children }: { children: React.ReactNode }) {
-  return <div className="grow bg-zinc-700 p-5 overflow-auto">{children}</div>
+  return <div className="grow bg-zinc-700 pb-1 p-5 overflow-auto">{children}</div>
 }
