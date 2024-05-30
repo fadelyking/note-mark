9393f3b927eefe68fa7f68162e8bfe3c673aebf4
fadelyking <fadely195@gmail.com>
2024-05-30 15:25:58 +0300

    create note title component

---
+++ src/renderer/src/components/FloatingNoteTitle.tsx
@@ -0,0 +1,9 @@
+export default function FloatingNoteTitle() {
+  const title = 'Note Title'
+
+  return (
+    <div className="flex justify-center">
+      <span className="text-gray-400">{title}</span>
+    </div>
+  )
+}
