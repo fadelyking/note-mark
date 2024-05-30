39aa8f32043fe20d742cf53276f8678a8177fc4a
fadelyking <fadely195@gmail.com>
2024-05-30 14:40:37 +0300

    overflow auto to add scroll when text overflows

--- src/renderer/src/components/Content.tsx
+++ src/renderer/src/components/Content.tsx
@@ -1,3 +1,3 @@
 export default function Content({ children }: { children: React.ReactNode }) {
-  return <div className="grow bg-zinc-700 p-5">{children}</div>
+  return <div className="grow bg-zinc-700 p-5 overflow-auto">{children}</div>
 }
