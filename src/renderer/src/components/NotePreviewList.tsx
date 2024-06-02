a07bcd5e2ebc78ce55aede774fc6dfb1c3385625
fadelyking <fadely195@gmail.com>
2024-06-02 10:04:40 +0300

    highlight selected note

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,7 +1,11 @@
+import { useNotesList } from '@renderer/hooks/useNotesList'
 import { notes } from '../store/mocks/index'
 import { NotePreview } from './NotePreview'
 import uuid4 from 'uuid4'
+
 export default function NotesPreviewList() {
+  const { notes, selectedNoteIndex, handleNoteSelect } = useNotesList({})
+
   if (notes.length === 0) {
     return (
       <ul className="text-center pt-4">
@@ -11,9 +15,13 @@ export default function NotesPreviewList() {
   }
   return (
     <ul className="mt-3 space-y-1">
-      {notes.map((note) => (
+      {notes.map((note, index) => (
         <li key={uuid4()}>
-          <NotePreview {...note} />
+          <NotePreview
+            isActive={selectedNoteIndex === index}
+            onClick={handleNoteSelect(index)}
+            {...note}
+          />
         </li>
       ))}
     </ul>
